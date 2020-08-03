3166387da054ba1359e8d6b425fb20bd5d86a13d
Tianhui Li <li.tianhu@husky.neu.edu>
2020-08-03 15:36:45 -0700

    modify architecture: separate UI and API

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-use-before-define */
 const fs = require('fs');
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { MongoClient } = require('mongodb');
 
 // replace this url
-const url = 'mongodb+srv://<username>:<password>@cluster0.w1lxr.mongodb.net/masterchef?retryWrites=true';
+// const url = 'mongodb+srv://<username>:<password>@cluster0.w1lxr.mongodb.net/masterchef?retryWrites=true';
+const url = 'mongodb+srv://tianhui:3255713988@cluster0.w1lxr.mongodb.net/masterchef?retryWrites=true';
 
 let db;
 
-let aboutMessage = "Master Chef API v1.0";
+const aboutMessage = 'Master Chef API v1.0';
 
 const resolvers = {
   Query: {
@@ -28,6 +31,15 @@ const resolvers = {
   }
 };
 
+async function getNextSequence(name) {
+  const result = await db.collection('counters').findOneAndUpdate(
+    { _id: name },
+    { $inc: { current: 1 } },
+    { returnOriginal: false },
+  );
+  return result.value.current;
+}
+
 async function recipeList() {
   const recipes = await db.collection('recipes').find({}).toArray();
   return recipes;
@@ -38,42 +50,45 @@ async function userList() {
   return users;
 }
 
-async function author({author}) {
-  const list = await userList();
-  return list.find((user) => {
-    return user.name === author;
-  })
+function author({author}) {
+  return db.collection('users').findOne({ name: { $eq : author } });
 }
 
-async function posts({name}) {
-  const list = await recipeList();
-  return list.filter((recipe) => {
-    return recipe.author === name;
-  })
+function posts({name}) {
+  return db.collection('recipes')
+    .find({ author: { $eq : name } }).toArray();
 }
 
-function createRecipe(_, {recipe}) {
-  const userExist = usersDB.some((user) => user.name === recipe.author);
-  if (!userExist) {
+
+// 使用counter来计算id - delete的时候也要减counter
+async function createRecipe(_, {recipe}) {
+  const userCount = await db.collection('users')
+    .countDocuments({ name : { $eq : recipe.author }});
+  if (userCount === 0) {
     throw new Error('User not found');
   }
   recipe.created = new Date().toDateString();
-  recipe.id = recipesDB.length + 1;
-  recipesDB.push(recipe);
-  return recipe;
+  recipe.id = await getNextSequence('recipes');
+  const result = await db.collection('recipes').insertOne(recipe);
+  const savedRecipe = await db.collection('recipes')
+    .findOne({ _id: result.insertedId });
+  return savedRecipe;
 }
 
-function createUser(_, {name, email}) {
-  const nameToken = usersDB.some((user) => user.name === name);
-  if (nameToken) {
+async function createUser(_, {name, email}) {
+  const userCount = await db.collection('users')
+    .countDocuments({ name : { $eq : name }});
+  if (userCount !== 0) {
     throw new Error('username exists');
   }
   const user = {
     name: name,
     email: email,
   }
-  usersDB.push(user);
-  return user;
+  const result = await db.collection('users').insertOne(user);
+  const savedUser = await db.collection('users')
+    .findOne({ _id: result.insertedId });
+  return savedUser;
 }
 
 async function connectToDb() {
@@ -90,15 +105,13 @@ const server = new ApolloServer({
 
 const app = express();
 
-app.use(express.static('public'));
-
 server.applyMiddleware({ app, path: '/graphql' });
 
 (async function () {
   try {
     await connectToDb();
     app.listen(3000, function () {
-      console.log('App started on port 3000');
+      console.log('API started on port 3000');
     });
   } catch (err) {
     console.log('ERROR:', err);
